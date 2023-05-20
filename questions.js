a7b6f7d2bd0f51a1d57d750909d48ed75c35f181
itiansrina <119058627+itiansrina@users.noreply.github.com>
2023-05-20 14:55:53 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In CSS, h1 can be called as?",
+    answer: "Selector",
+    options: [
+      "selector",
+      "value",
+      "attribute",
+      "tag"
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: "Style",
+    options: [
+      "Css",
+      "Text/style",
+      "Style",
+      "Script"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ {
+  numb: 6,
+  question: "CSS padding property is used for?",
+  answer: "Space",
+  options: [
+    "Border",
+    "Space",
+    "Margin",
+    "Background Color"
+  ]
+},
+{
+  numb: 7,
+  question: "In CSS, “font-size” can be called as?",
+  answer: "Property-Name",
+  options: [
+    "Selector",
+    "Property",
+    "Rule",
+    "Property-Name"
+  ]
+},
+{
+  numb: 8,
+  question: "Which of these tags would display the largest text?",
+  answer: "h6",
+  options: [
+    "p",
+    "h1",
+    "h5",
+    "h6"
+  ]
+},
+{
+  numb: 9,
+  question: "How can you make a bulleted list?",
+  answer: "ul",
+  options: [
+    "list",
+    "ol",
+    "ul",
+    "dl"
+  ]
+},
+{
+  numb: 10,
+  question: "You should save HTML files with which file extension?",
+  answer: ".html",
+  options: [
+    ".htm",
+    ".webpage",
+    ".html",
+    ".index"
+  ]
+},
+
+];
+
+
+
